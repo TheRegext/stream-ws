c53b754df8ffeb74540c9d65700cdb1150290df4
Brian Lara <brian.lara@outlook.com.ar>
2024-06-16 22:16:38 -0300

    Ciclo del menu

--- menu.cpp
+++ menu.cpp
@@ -5,21 +5,23 @@ using namespace std;
 void menu()
 {
     int opcion;
+    
+    do{
+        cout << "MENU PRINCIPAL!"<< endl;
+        cout << "1 - UN JUGADOR" << endl;
+        cout << "2 - DOSJUGADOR" << endl;
+        cout << "3- ESTADISTICAS" << endl;
+        cout << "4- CREDITOS" << endl;
+        cout << "0- SALIR" << endl;
 
-    cout << "MENU PRINCIPAL!"<< endl;
-    cout << "1 - UN JUGADOR" << endl;
-    cout << "2 - DOSJUGADOR" << endl;
-    cout << "3- ESTADISTICAS" << endl;
-    cout << "4- CREDITOS" << endl;
-    cout << "0- SALIR" << endl;
+        cin >> opcion;
 
-    cin >> opcion;
+        switch(opcion)
+        {
 
-    switch(opcion)
-    {
-
-    case 0:
-        return;
-        break;
-    }
+        case 0:
+            return;
+            break;
+        }
+    }while(opcion != 0);
 }
